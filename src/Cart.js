189173b028a401356ceed8fa5faaea55662856db
SoNuPandey22 <pandey.sonu1739@gmail.com>
2021-05-13 19:01:52 +0530

    updated

--- src/Cart.js
+++ src/Cart.js
@@ -2,12 +2,55 @@ import React from 'react'
 import CartItem from './CartItem'
 
 class Cart extends React.Component {
+	constructor(){
+    	super();
+    	this.state = {
+    		products: 
+    		[
+	    		{
+	    		 id:1,
+	             price: 999,
+	    		 title: 'phone',
+	    		 qty: 1,
+	    		 img: 'https://i.pinimg.com/736x/53/ff/05/53ff0591adc2b77ac000aca68791365a.jpg'
+	    	    },
+	    	    {
+	    	     id:2,
+	             price: 1000,
+	    		 title: 'mobile',
+	    		 qty: 1,
+	    		 img: 'https://i.pinimg.com/736x/53/ff/05/53ff0591adc2b77ac000aca68791365a.jpg'
+	    	    },
+	    	    {
+	    	     id:3,
+	             price: 200,
+	    		 title: 'laptop',
+	    		 qty: 1,
+	    		 img: 'https://i.pinimg.com/736x/53/ff/05/53ff0591adc2b77ac000aca68791365a.jpg'
+	    	    }
+
+    		]
+    	}
+
+    }
+    // fucntion to increae the quantity
+    handleIncreaseQty = (product)=>{
+       console.log('quantity badha de iski ')
+    }
   render() {
+  	const { products } = this.state;
   	return(
   		<div className='cart'>
-	        <CartItem/>,
-	        <CartItem/>,
-	        <CartItem/>,
+	        {products.map((product)=>{
+	        	return (
+	        		<CartItem 
+	        		    products={product} 
+	        		    key={product.id} 
+	        		    handleIncreaseQty = {this.handleIncreaseQty}
+	        		/>
+	        		)
+	        	
+	        })}
 
         </div>
 
--- src/CartItem.js
+++ src/CartItem.js
@@ -2,59 +2,12 @@ import React from 'react'
 
 
 class CartItem extends React.Component {
-    constructor(){
-    	super();
-    	this.state = {
-    		price: 999,
-    		title: 'phone',
-    		qty: 1,
-    		img: 'https://i.pinimg.com/736x/53/ff/05/53ff0591adc2b77ac000aca68791365a.jpg'
-    	}
+    
 
-    }
-
-    testing = () =>{
-    	const promise = new Promise((resolve, reject)=>{
-    		setTimeout(()=>{
-    			resolve('done');
-    		}, 6000)
-    	})
-
-    	promise.then(()=>{
-    		this.setState({ qty: 100 })
-
-    		console.log(this.state);
-    	})
-    }
-    increaseQty = ()=> {
-		this.setState((prevState)=>{
-           return{
-           	 qty : prevState.qty + 1           }
-		}, ()=>{
-			console.log(this.state);
-		})
-	}
-	decreaseQty = ()=> {
-		 // shalow merging
-		// this.setState({
-		// 	qty : this.state.qty+1,
-		// })
-
-	
-	    this.setState((prevState)=>{
-	    	if(prevState.qty === 0) {
-	    		return;
-	    	}
-	    	return {
-	    	 qty : prevState.qty-1,
-
-	    	}
-	    })
-	}
 
 	render() {
-		const{ price, title, qty, img } = this.state;
-		// console.log(img);
+		console.log(this.props)
+		const{id, price, title, qty, img } = this.props.products;
 		
 		return(
 			<div className='cart-item'>
@@ -71,7 +24,7 @@ class CartItem extends React.Component {
 	                src='https://t4.ftcdn.net/jpg/00/70/16/29/240_F_70162903_5mFpUbO3ZfRyD4gslH8j2c5VxjGMKU9G.jpg' 
 	                alt='' 
 	                className='action-icons'
-	                onClick={ this.increaseQty }
+	                onClick={ this.props.handleIncreaseQty }
 	              />
 	              <img src='https://t3.ftcdn.net/jpg/03/73/49/86/240_F_373498649_nBxauQ0ipBSVrVcMpWWVmTpXu3BLvRyY.jpg'  
 	                alt='' 
