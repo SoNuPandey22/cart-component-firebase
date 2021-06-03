f4b0c4bc47630a7c4411cb158c963a597d1998b6
SoNuPandey22 <pandey.sonu1739@gmail.com>
2021-06-03 16:06:59 +0530

    cart counter ready

---
+++ src/digitToString.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <string>
+using namespace std; 
+
+char arr[] = ("zero","one", "two", "three","four", "five", "six", "seven", "eight", "nine");
+int main() {
+
+	cout<<arr[6]<<endl;
+	return 0;
+	
+}
--- src/index.js
+++ src/index.js
@@ -2,12 +2,27 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import * as firebase from 'firebase';
+import 'firebase/firestore';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+  var firebaseConfig = {
+    apiKey: "AIzaSyBLumo8undwqyXTw7rj1GcgaOkKZUHIq-M",
+    authDomain: "cartproject-503e7.firebaseapp.com",
+    projectId: "cartproject-503e7",
+    storageBucket: "cartproject-503e7.appspot.com",
+    messagingSenderId: "476152543800",
+    appId: "1:476152543800:web:10126ef18f3f1105a104cd",
+    measurementId: "G-T0DDD4VYG1"
+  };
+  // Initialize Firebase
+
+  firebase.initializeApp(firebaseConfig);
+   firebase.analytics();
+  const db = getFirestore();
+  console.log('db',db);
+  // console.log('from index file', a); ,making the connection succes
+  // firebase.analytics();
+
+ReactDOM.render(<App />, document.getElementById('root'));
 
 
